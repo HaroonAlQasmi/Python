df7564264bb7b79b40818f9b3bfb91718e88b3b7
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-02-27 10:16:49 +0400

    Added a 12321 pattern

--- PrintingPatterns.py
+++ PrintingPatterns.py
@@ -11,3 +11,35 @@ for num in range(1,user_input + 1):
     spaces = " " * (user_input-num)
     stars = "*" * (2*num-1)
     print(spaces+stars)
+
+"""
+1           (10^0 + 0) * (10^0 + 0)     
+121         (10^1 + 1) * (10^1 + 1)
+12321       (10^2 + 11) * (10^1 + 11)
+1234321     (10^3 + 111) * (10^3 + 111)
+123454321   (10^4 + 1111) * (10^4 + 1111)
+"""
+import math
+
+no_of_rows = 5
+prev_res = 0
+count = 0
+
+# curr_res = math.pow(10, count) + prev_res  # 1 + 0 (count = 0)
+# count += 1
+# prev_res = curr_res
+# print(curr_res)  # 1
+
+# curr_res = math.pow(10, count) + prev_res # 10 + 1 = 11  (count = 1)
+# count += 1
+# prev_res = curr_res
+# print(curr_res)  # 11
+
+# curr_res = math.pow(10, count) + prev_res # 100 + 11 = 111  (count = 2)
+# count += 1
+# prev_res = curr_res
+# print(int(curr_res))  # 11
+
+for line_no in range(no_of_rows):
+    prev_res = math.pow(10, line_no) + prev_res 
+    print(int(prev_res) * int(prev_res))
